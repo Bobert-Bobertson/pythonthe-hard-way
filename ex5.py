81bf18332ec88026f93ad9f880e6ad6d1bc4d4bb
Bobert Bobertson <baladadczedrick@gmail.com>
2024-09-16 19:02:59 -0700

    Exercise 5
    Introduce format strings

---
+++ ex5.py
@@ -0,0 +1,22 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74 #in inches
+weight = 180 # in lbs
+eyes = 'Blue'
+hair = 'Brown'
+teeth = 'White'
+print(f"Let's talk about {name}. ")
+
+print(f"He's {height} inches tall. ")
+
+print(f"He's {weight} lbs heavy. ")
+
+print(f"He's got {eyes} eyes and {hair} hair")
+
+print(f"He's his teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f" If I add {age} , {height} , and {weight} I get {total}.")
+
+
+
